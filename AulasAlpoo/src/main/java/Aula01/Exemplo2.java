134805c5e1c63e51d6f9bbe951675a0a77a41c33
Andrey Paceli <andofmen@gmail.com>
2020-08-24 22:58:26 -0300

    Adicionado Exemplos
    Exemplo 1 e 2, adicionados no packed "Aula01"

---
+++ AulasAlpoo/src/main/java/Aula01/Exemplo2.java
@@ -0,0 +1,30 @@
+package aula01;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Exemplo2 extends JFrame {
+    
+    String firstNumber;
+    String secondNumber;
+    int number1;
+
+    public Exemplo2() {
+        
+        firstNumber = JOptionPane.showInputDialog(this, "Entre com o Primeiro número:");
+        secondNumber = JOptionPane.showInputDialog(this, "Entre com o Segundo Número:");
+
+        int number1 = Integer.parseInt(firstNumber);
+        int number2 = Integer.parseInt(secondNumber);
+
+        int sum = number1 + number2;
+
+        JOptionPane.showMessageDialog(this, "A soma dos número é: " + sum);
+
+    }
+
+    public static void main(String[] args) {
+        Exemplo2 obj = new Exemplo2();
+        System.out.println("Saiu do construtor");
+    }
+}
